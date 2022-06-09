31abcf341bf857c0e5b8802c561e1682d85a0f30
Erick Cristian dos Santos <erick.cristians@gmail.com>
2022-06-09 20:19:56 -0300

    Commit inicial

---
+++ main.js
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby} from "./funcoes.mjs"
+
+console.log(mostraIdade('Erick', 35));
+console.log(mostraCidade('Erick', 'Betim'));
+console.log(mostraHobby('Erick', 'Estudar Programação'));
